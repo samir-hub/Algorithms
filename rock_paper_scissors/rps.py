df77e0cd20b2780de3dc2d26909b3b327c92ce47
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-01-22 19:53:43 -0500

    working on RPS

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,17 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
+    other_array = []
+    my_array = [['rock'], ['paper'], ['scissors']]
+    if len(other_array) == 3 ** n:
+      return other_array
+    else: 
+        for i in my_array:
+            other_array.append(i)
+            print(other_array)
+            return rock_paper_scissors(n-1)    
+   
+    return other_array
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -12,3 +21,20 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+
+other_array = []
+my_array = [['rock'], ['paper'], ['scissors']]
+
+for i in my_array:
+    for i in my_array:
+        for i in my_array:
+            other_array.append(i+i+i)
+    
+print(len(other_array))    
+
+
+#rock_paper_scissors(3)
+
+
+ 
